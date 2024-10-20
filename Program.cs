7d77823bf3f5e00f4249ba81fbffe7736848f1da
Sumaya-10 <sumayam-ab108@code.edu.az>
2024-10-20 23:54:33 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,35 +1,15 @@
-﻿//1ci task
-namespace Product_1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Product product = new Product("Tshirt", "Koton", 30);
-            product.PrintInfo();
-            /*Product[] products = new Product[]
-            {
-                new Product("Telefon", "Samsung", 999.99),
-                new Product("Laptop", "Dell", 1500.00),
-                new Product("Qulaqlıq", "Apple", 299.99),
-                new Product("Televizor", "LG", 850.00)
-            };
-            double minPrice = 300.00;
-            double maxPrice = 1000.00;
-            Product[] Mehsullar = QiymetMehsul (products, minPrice, maxPrice);
-            }
-            foreach (Product product in QiymetMehsul )
-            {
-                product.ShowProductInfo(); 
-            }
-            static  Product[] QiymetMehsul(string[] args, double minPrice, double maxPrice)
-            {
-                Product[] QiymetMehsul = products
-                .Where(p => p.Price >= minPrice && p.Price <= maxPrice)
-                .ToArray();
+﻿namespace _20okttask;
 
-                return QiymetMehsul;
-            }*/
-        }
+ class Program
+{
+    static void Main(string[] args)
+    {
+        Singer singer = new Singer("Maral ","Tahirqizi ",180);
+
+        Console.WriteLine(singer.name  + " " + singer.Surname  + " " + singer.Age);
+
+        Song song = new Song("Summertime Sadness", "pop", "Maral");
+
+        Console.Write(song.songName + " " + song.genre + " " + song.singer);
     }
 }
---
+++ Song.cs
@@ -0,0 +1,55 @@
+﻿namespace _20okttask;
+
+ class Song
+{
+    public string songName;
+    public string genre;
+    public string singer;
+    public int[] ratings = new int[5];
+    public int ratingsCount = 0;
+
+    public Song(string name, string genre, string singer)
+    {
+        this.songName = name;
+        this.genre = genre;
+        this.singer = singer;
+    }
+    public string name
+    {
+        get {  return songName; }
+        set
+        {
+            if (songName.Length < 101)
+                this.songName = name;
+            else
+                Console.WriteLine("yanlisdir");
+        }
+    }
+    public void AddRating(int rating)
+    {
+        if(ratingsCount < ratings.Length)
+        {
+            rating = ratings[rating];
+            ratingsCount++;
+        }
+        else
+        {
+            Console.WriteLine("reytinqe dusmuyub");
+        }
+    }
+    /*public int GetAverageRating()
+    { 
+        return ratingsCount;
+        int sum = 
+    }*/
+
+    public string Genre
+    {
+        get { return genre; }
+        set
+        {
+            if(Genre=="pop" || Genre == "jazz" || Genre == "rock")
+                genre = Genre;
+        }
+    }
+}
