0455052c8c87bf3b2d63bd2ce8dde46201f59d28
Sumaya-10 <sumayam-ab108@code.edu.az>
2024-10-24 00:32:39 +0400

    Add files via upload

---
+++ indexer/ListInt.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq.Expressions;
+
+namespace Indexertask;
+
+class ListInt
+{
+    private int[] array;
+    private int count;
+    public ListInt()
+    {
+        array = new int[0];
+        count = 0;
+    }
+    public int this[int index]
+    {
+        get
+        {
+            if (index > 0 || index < count)
+                return array[index];
+            throw new IndexOutOfRangeException();
+        }
+        set
+        {
+            if (value > 0 || value < count)
+                array[index] = value;
+            else
+                throw new IndexOutOfRangeException();
+        }
+    }
+    public void Add(int num)
+    {
+         Array.Resize(ref array, count + 1);
+        array[count] = num;
+        count++;
+    }
+    public void Add(params int[] nums)   //- Göndərilən parametrləri arraya daxil edir.
+    {
+        int Count = count;
+        count += nums.Length;
+        Array.Resize(ref array, count);
+        Array.Copy(nums, 0, array, Count, nums.Length);
+    }
+    public bool Contains(int num)  //Göndərilən ədəd array-da varsa true yoxdursa false qaytarır.
+    {
+        for (int i = 0; i < count; i++) 
+        {
+            if (array[i] == num) 
+            {
+                return true; 
+            }
+        }
+        return false; 
+
+    }
+    public int Pop()     // - Array-ın sonuncu elementini arraydan silir və həmin elementi geri qaytarır.
+    {
+        if (count == 0)
+            Console.WriteLine("array bosdur");
+        Array.Resize(ref array, count - 1);
+        int son = array[count - 1];
+        count--;
+        return son;
+    }
+    public int Sum()
+    {
+        int cem = 0; 
+        for (int i = 0; i < count; i++) 
+        {
+            cem += array[i];
+        }
+        return cem; 
+    }
+    public override string ToString()
+    {
+        return string.Join(", ", array);
+    }
+    
+    
+}
---
+++ indexer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Indexertask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListInt list = new ListInt();
+
+            list.Add(1);
+            list.Add(2, 3, 4);
+            Console.WriteLine(list.ToString()); 
+            
+            Console.WriteLine(list.ToString()); 
+                Console.WriteLine(list.Contains(3)); 
+            
+
+            Console.WriteLine(list.Sum()); 
+            
+
+            int last = list.Pop();
+
+
+            Console.WriteLine(last); 
+            Console.WriteLine(list.ToString());
+        }
+    }
+}
