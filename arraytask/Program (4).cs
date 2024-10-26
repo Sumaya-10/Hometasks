5cb46b9922c740066ad1c599f32387b3ce573ad7
Sumaya-10 <sumayam-ab108@code.edu.az>
2024-10-26 18:35:34 +0400

    Add files via upload

---
+++ arraytask/Program (4).cs
@@ -0,0 +1,30 @@
+﻿//3. Ədədlərdən ibarət arrayda neçə rəqəm olduğunu ekrana çap edin
+int[] numbers = { 5, 4, 13 };
+int reqemin sayi = 0;
+while( i > 0 & i < 10) 
+{
+    reqemin sayi ++;
+    
+}
+Console.WriteLine(reqemin sayi);
+
+
+//1ci task
+/*int k = 2;
+int g = 4;
+int son = 1;
+for (int i = 0; i < g; i++)     //2**0=1 
+{
+    son = son * k;
+}
+       Console.WriteLine(son);*/
+
+//2ci task
+/*int num = 67;
+int m = 0;
+while (num > 0)
+{
+    m = m + 1;
+    num = num / 10;
+}
+Console.WriteLine(m);*/
