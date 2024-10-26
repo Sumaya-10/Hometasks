7739cadb820b62bbe074b6b4cde93c24dcefdef7
Sumaya-10 <sumayam-ab108@code.edu.az>
2024-10-26 18:10:46 +0400

    Add files via upload

---
+++ class/Program (2).cs
@@ -0,0 +1,15 @@
+﻿namespace _20okttask;
+
+ class Program
+{
+    static void Main(string[] args)
+    {
+        Singer singer = new Singer("Maral ","Tahirqizi ",180);
+
+        Console.WriteLine(singer.name  + " " + singer.Surname  + " " + singer.Age);
+
+        Song song = new Song("Summertime Sadness", "pop", "Maral");
+
+        Console.Write(song.songName + " " + song.genre + " " + song.singer);
+    }
+}
